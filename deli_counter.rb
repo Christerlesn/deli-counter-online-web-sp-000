9537930f48d2e19761e3d3f76bd240450b99fea0
Christerle Nounoute <christerlesn@gmail.com>
2020-09-07 18:35:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,13 @@
 katz_deli = ["Mary", "Brandon"]
 
 def line(index)
-  if index == []
-    puts "The line is empty"
-  else index.each_with_index do |val, index|
-  puts "#{val} #{index}"
-end
-end
+  line_array = []
+  if index.length == 0
+    puts "The line is currently empty."
+  else
+x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
 end
