934748437ec666e5907ed4b3890a3369ba98dfe7
Christerle Nounoute <christerlesn@gmail.com>
2020-09-07 18:26:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(index)
     if x == []
     puts "The line is empty"
   else
-index.each_with_index {|val, index -1| puts "#{val} #{index}"}
+index.each_with_index {|val, index| puts "#{val} #{index}"}
+end
 end
 end
