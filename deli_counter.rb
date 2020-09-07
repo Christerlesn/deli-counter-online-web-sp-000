cdda48c20afe921e2065e4a0d98c971bc0266016
Christerle Nounoute <christerlesn@gmail.com>
2020-09-07 19:00:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,11 @@ def take_a_number (katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(array)
+  if array > 0
+  puts "Currently serving #{array[0]}"
+  array.shift
+else
+  puts "There is nobody waiting to be served!"
+end
