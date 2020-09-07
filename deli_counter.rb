31093bb6f8fb5112c2f30c35e9996352c8cf163e
Christerle Nounoute <christerlesn@gmail.com>
2020-09-07 18:31:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,7 @@ katz_deli = ["Mary", "Brandon"]
 def line(index)
   if index == []
     puts "The line is empty"
-  else
-index.each_with_index do |val, index|
-index - 1
+  else index.each_with_index do |val, index - 1|
   puts "#{val} #{index}"
 end
 end
